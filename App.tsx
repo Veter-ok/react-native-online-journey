06ce6b2baad6c2c2a805c596d2b7e041336a2155
Lavrov Rodion <61391385+Veter-ok@users.noreply.github.com>
2023-02-28 17:36:53 +0300

    Preparation for YaMap

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import Navigation from './Navigation/Navigation';
 import AuthProvider from './providers/AuthProvider';
 import YaMap from 'react-native-yamap'
 
-// YaMap.init('d46dd253-2d53-4000-bbc2-6aa417d5fcd8');
+YaMap.init('');
 
 export default function App() {
   return (
